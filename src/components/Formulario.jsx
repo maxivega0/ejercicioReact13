c3669b4adc23d78903e9c340b7e0a201edec643f
maxivega0 <maximilianovega600@gmail.com>
2023-06-13 00:38:53 -0300

    formulario cargando datos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,22 +7,33 @@ const Formulario = () => {
   const [pais, setPais] = useState("");
   const [ciudad, setCiudad] = useState("");
 
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCiudad(e.target.value);
+    setPais(e.target.value);
+    console.log(pais);
+    console.log(ciudad);
+  };
   return (
     <div>
       <Container>
-        <Form >
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="ubicacion">
             <Form.Label>Ingrese su pais:</Form.Label>
             <Form.Control
               type="text"
               placeholder="Argentina"
-              
+              onChange={(e) => setPais(e.target.value)}
+              value={pais}
             />
             <Form.Label>Ingrese su ciudad:</Form.Label>
             <Form.Control
               type="text"
               placeholder="Buenos Aires"
-              
+              onChange={(e) => setCiudad(e.target.value)}
+              value={ciudad}
             />
             <Button variant="warning" type="submit">
               Buscar
